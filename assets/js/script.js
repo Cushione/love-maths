27a526902d28f933f0afbbff9399110d33504383
Cushione <maulerovak@gmail.com>
2022-10-20 06:23:57 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function IncrementScore() {
+    
+}
+
+function IncrementWrongAnswer() {
+    
+}
+
+function displayAdditionAnswer() {
+    
+}
+
+function displaySubtractAnswer() {
+    
+}
+
+function displayMultiplyAnswer() {
+    
+}
